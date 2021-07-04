b18cc498e25e49816cc754741d33a2d02bc47f36
anmolsrivastava <as248@rice.edu>
2021-07-04 13:26:06 -0500

    adding application load tests

---
+++ src/test/java/net/esportsbets/ESportsBetsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.esportsbets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESportsBetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
