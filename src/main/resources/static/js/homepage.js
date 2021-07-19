6b3a259780c22467b47425ef0eb3b4c5e45b64b7
Bryan Boone <bmboone7@gmail.com>
2021-07-19 03:52:09 -0500

    I am committing

---
+++ src/main/resources/static/js/homepage.js
@@ -0,0 +1,83 @@
+$("resultsDisplay1").ready(function () {
+    $.get( "/matches/results/count").done(
+        function ( data ) {
+            console.log("pages: ", data)
+        }
+    )
+    $.get( "/matches/results?page=0").done(
+        function ( data ) {
+            populateResults(data)
+        }
+    )
+});
+
+function populateResults(matches) {
+    var rows = "";
+    matches.forEach(match=>{
+        rows += populateRow(match);
+        $("#resultsDisplay").html(rows);
+    });
+    $("#resultsDisplay").html(rows);
+}
+
+function populateRow(match) {
+    divText = ''
+    divText+='<div class="col-lg-9"\n'
+    divText+='     style="margin:auto; padding:20px; border-style:solid; border-width:2px; border-color:lightgray; border-radius:10px;">\n'
+    divText+='    <div class="container">\n'
+    divText+='        <div class="row">\n'
+    divText+='            <div class="col-lg-3">\n'
+    divText+='                <img src="images/halo5_maps/' + match.map + '.png" width="100" height="100"\n'
+    divText+='                     style="background-image: url(\'images/game_icon.png\'); background-size: 100px auto; height: 100px; width: auto;">\n'
+    divText+='            </div>\n'
+    divText+='            <div class="col-lg-8">\n'
+    divText+='                <div class="row">\n'
+    divText+='                    <div class="col-sm-6">\n'
+    divText+='                        Time:\n'
+    divText+='                        <span id="time">' + match.time +'</span>\n'
+    divText+='                    </div>\n'
+    divText+='                </div>\n'
+    divText+='                <div class="row">\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        Team\n'
+    divText+='                    </div>\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        Score\n'
+    divText+='                    </div>\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        Winner\n'
+    divText+='                    </div>\n'
+    divText+='                </div>\n'
+    divText+='                <div class="row '
+    divText+=                               (match.team1.result=="Win")?'alert-success':'alert-danger'
+    divText+=                                '">\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        ' + match.team1.teamName + '\n'
+    divText+='                    </div>\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        ' + match.team1.score + '\n'
+    divText+='                    </div>\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        ' + match.team1.result + '\n'
+    divText+='                    </div>\n'
+    divText+='                </div>\n'
+    divText+='                <div class="row '
+    divText+=                                   (match.team2.result=="Win")?'alert-success':'alert-danger'
+    divText+=                                   '">\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        ' + match.team2.teamName + '\n'
+    divText+='                    </div>\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        ' + match.team2.score + '\n'
+    divText+='                    </div>\n'
+    divText+='                    <div class="col-sm-3">\n'
+    divText+='                        ' + match.team2.result + '\n'
+    divText+='                    </div>\n'
+    divText+='                </div>\n'
+    divText+='            </div>\n'
+    divText+='        </div>\n'
+    divText+='    </div>\n'
+    divText+='</div>\n'
+
+    return divText;
+}
