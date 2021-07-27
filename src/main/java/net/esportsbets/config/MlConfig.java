258d3a6942892a67fe514101c180a45e2bf2274b
sawyerbro <sawyer.broussard@gmail.com>
2021-07-27 16:36:23 -0500

    add MlConfig.java

---
+++ src/main/java/net/esportsbets/config/MlConfig.java
@@ -0,0 +1,46 @@
+package net.esportsbets.config;
+
+import net.esportsbets.ESportsBetsApplication;
+import org.pmml4s.model.Model;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MLConfig {
+
+    @Bean("moneylineModel")
+    public Model moneylineModel() {
+        return Model.fromFile(
+                ESportsBetsApplication.class.getClassLoader().getResource(
+                        "public/pmml/moneyline_bets.pmml").getFile());
+    }
+
+    @Bean("slayerSpreadModel")
+    public Model slayerSpreadModel() {
+        return Model.fromFile(
+                ESportsBetsApplication.class.getClassLoader().getResource(
+                        "public/pmml/slayer_spread.pmml").getFile());
+    }
+
+    @Bean("strongholdSpreadModel")
+    public Model strongholdSpreadModel() {
+        return Model.fromFile(
+                ESportsBetsApplication.class.getClassLoader().getResource(
+                        "public/pmml/stronghold_spread.pmml").getFile());
+    }
+
+    @Bean("oddballSpreadModel")
+    public Model oddballSpreadModel() {
+        return Model.fromFile(
+                ESportsBetsApplication.class.getClassLoader().getResource(
+                        "public/pmml/oddball_spread.pmml").getFile());
+    }
+
+    @Bean("ctfSpreadModel")
+    public Model ctfSpreadModel() {
+        return Model.fromFile(
+                ESportsBetsApplication.class.getClassLoader().getResource(
+                        "public/pmml/ctf_spread.pmml").getFile());
+    }
+
+}
