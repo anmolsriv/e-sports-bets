1196d2fa7c822cf6f09b70bebb34e979ae2c51c9
sawyerbro <sawyer.broussard@gmail.com>
2021-07-30 13:19:17 -0500

    enable player search from any page

--- src/main/resources/static/js/players.js
+++ src/main/resources/static/js/players.js
@@ -70,6 +70,11 @@ function getGamertagsInTable() {
 function handleClickGamertag(gamertag) {
   let gamertags = getGamertagsInTable();
   if (gamertags.indexOf(gamertag) === -1) {
+    let url = window.location.href.split("/")
+    if (url[url.length - 1] != "players") {
+      setCookie("gamertag", gamertag, 0.003)
+      window.location.href = window.location.origin + "/players";
+    }
     getPlayerStatistics(gamertag);
   }
   let searchInput = document.getElementById("searchInput");
@@ -136,25 +141,35 @@ function addPlayersToTable(players) {
   }
 }
 
-function getOdds(matches) {
-  $.ajax({
-    url: "/odds",
-    data: JSON.stringify(matches),
-    type: 'POST',
-    traditional: true,
-    contentType: "application/JSON",
-    success: function ( data) {
-      console.log(data)
-    }
-  })
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 window.onload = function() {
   buildPlayersTable();
-  // getOdds([ {'001fc61d-e1ac-4d2e-86d0-a8354047806a': 'slayer'}, {'00208d69-dabc-400e-99b5-529535ef6962': 'slayer'} ])
-  getOdds([ {'matchId': '001fc61d-e1ac-4d2e-86d0-a8354047806a', 'gameType': 'slayer'} ])
-  // getOdds(['001fc61d-e1ac-4d2e-86d0-a8354047806a'], "strongholdSpread")
-  // getOdds(['001fc61d-e1ac-4d2e-86d0-a8354047806a'], "oddballSpread")
-  // getOdds(['001fc61d-e1ac-4d2e-86d0-a8354047806a'], "ctfSpread")
-  // getOdds(['001fc61d-e1ac-4d2e-86d0-a8354047806a'], "moneyline")
+  let gamertag = getCookie("gamertag");
+  if (gamertag != null) {
+    getPlayerStatistics(gamertag);
+    eraseCookie("gamertag");
+  }
+
 };
