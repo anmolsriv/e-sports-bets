0c8d3034daf3d9ad5497ecaf01f7bdd82e348e8c
sawyerbro <sawyer.broussard@gmail.com>
2021-08-14 16:27:16 -0500

    add heroku dependencies

--- src/main/java/net/esportsbets/ESportsBetsApplication.java
+++ src/main/java/net/esportsbets/ESportsBetsApplication.java
@@ -10,6 +10,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 public class ESportsBetsApplication {
 
+    /**
+     * Get the port assigned by Heroku, or return 4567 if ran locally
+     * @return default port if heroku-port isn't set (i.e. on localhost)
+     */
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(ESportsBetsApplication.class, args);
 	}
